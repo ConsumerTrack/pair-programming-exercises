22fbf31b029fafc0657b84855d1061246be3130e
Derek <derekrw@gmail.com>
2024-12-16 13:42:04 -0500

    Update index.js to remove test

--- nodejs/creative-api/routes/index.js
+++ nodejs/creative-api/routes/index.js
@@ -2,9 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 const creativesRouter = require('./creatives'); 
-router.get('/test', (req, res) => res.send("hellio"));
-
-
 router.use('/creatives', creativesRouter);
 
 module.exports = router;
